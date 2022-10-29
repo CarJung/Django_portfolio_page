8305dfc327b7ed7e64d11695d172af6ebd070255
CarJung <wrob.kw@gmail.com>
2022-10-29 12:54:16 +0200

    sa

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os as os
 from turtle import Turtle
+import tkinter as TK
 
 import dj_database_url
 import django_heroku
