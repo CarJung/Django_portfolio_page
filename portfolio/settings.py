41661fa043afd9762490291a86dd4a24b7dac26d
CarJung <wrob.kw@gmail.com>
2022-11-02 10:09:56 +0100

    debug

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,7 @@ PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'c9zp2mh&70b1$-y6ko4b!gdud((lr%+xg58%fm#dbmv1vz3y#f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://krzysztof-wrobel.herokuapp.com','127.0.0.1']
 
